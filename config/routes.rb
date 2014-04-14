813095c3a25dcb3ecc560cd2f037cb94268f1503
Sara Howden <lhsimpson@madisoncollege.edu>
2014-04-14 17:38:04 -0500

    Added footer and debug info.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Bookmarks::Application.routes.draw do
+  resources :users
   get "users/new"
   resources :bookmarks
   root  'bookmarks#index'
+  match '/signup',  to: 'users#new',            via: 'get'
   
 end
